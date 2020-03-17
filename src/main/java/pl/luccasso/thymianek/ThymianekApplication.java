95e0706e20348de31853b603cbe050165747063a
pikolinian <pikolinian@gazeta.pl>
2020-03-17 11:22:07 +0100

    App From Initializer

---
+++ src/main/java/pl/luccasso/thymianek/ThymianekApplication.java
@@ -0,0 +1,13 @@
+package pl.luccasso.thymianek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymianekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymianekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/luccasso/thymianek/ThymianekApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.luccasso.thymianek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymianekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
